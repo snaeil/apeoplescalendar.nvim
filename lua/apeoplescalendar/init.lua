6bf8da52191975a5c96da7bb16d185fc8969ca7e
Sandro Eiler <sandro.eiler@posteo.de>
2023-03-09 14:10:55 +0100

    feat: cache fetched events

--- lua/apeoplescalendar/init.lua
+++ lua/apeoplescalendar/init.lua
@@ -1,6 +1,24 @@
 local M = {}
 
-function os.capture(cmd, raw)
+local function generate_tmp_file_path()
+   local path = os.getenv("TMPDIR") or os.getenv("TEMP") or os.getenv("TMP") or "/tmp"
+   return path
+end
+
+local function test_service_connection()
+    local cmd = "ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ok || echo error | grep \"ok\""
+    local handle = io.popen(cmd)
+    if handle ~= nil then
+        local output = handle:read("*a")
+        handle:close()
+        if output:find("ok") then
+            return true
+        end
+    end
+    return false
+end
+
+local function command_capture(cmd, raw)
   local f = assert(io.popen(cmd, 'r'))
   local s = assert(f:read('*a'))
   f:close()
@@ -12,11 +30,23 @@ function os.capture(cmd, raw)
 end
 
 local function fetch_apc_events()
-    local api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0YWhtYXhmZmNxYW5raWVudWxoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTcwNDAzOTUsImV4cCI6MTk3MjYxNjM5NX0.-YZmaNQcoQXbC0_VZYD_jNuOgVbFEu9fbpL_lRDBIH0"
-    local request_url = string.format("https://stahmaxffcqankienulh.supabase.co/rest/v1/events?select=*&day=eq.%d&month=eq.%d&order=title.asc", os.date("%m"), os.date("%d") )
-    local curl_command = string.format("curl -s '%s' -H \"apikey: %s\"", request_url, api_key)
-    local curl_result = os.capture(curl_command, true)
-    return curl_result
+    local filename = generate_tmp_file_path() .. "/apeoplescalendar_".. os.date("%Y%m%d")
+    local result = nil
+    local file = io.open(filename, "r")
+    if file ~= nil then
+        result = file:read("*all")
+        file:close()
+    else
+        file = io.open(filename, "w")
+        if test_service_connection() == true then
+            local api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0YWhtYXhmZmNxYW5raWVudWxoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTcwNDAzOTUsImV4cCI6MTk3MjYxNjM5NX0.-YZmaNQcoQXbC0_VZYD_jNuOgVbFEu9fbpL_lRDBIH0"
+            local request_url = string.format("https://stahmaxffcqankienulh.supabase.co/rest/v1/events?select=*&day=eq.%d&month=eq.%d&order=title.asc", os.date("%m"), os.date("%d") )
+            local curl_command = string.format("curl -s '%s' -H \"apikey: %s\"", request_url, api_key)
+            result = command_capture(curl_command, true)
+            file:write(result)
+        end
+    end
+    return result
 end
 
 local function format_up_to_x(s, x, indent)
@@ -53,48 +83,57 @@ local function json_to_table(encoded)
     return decoded
 end
 
-function M.today_teaser()
-    local events = vim.json.decode(fetch_apc_events())
-    local event = events[ math.random( #events ) ]
-    local teaser = event["otd"]
-    if teaser ~= nil and teaser ~= "" then
-        teaser = format_up_to_x(teaser, 50)
-        require("notify").notify(teaser .. "\n \nSee all events of today with :APeoplesCalendar", "error", {
-          title = "APeoplesCalendar",
-          timeout = 8000,
-          render = "simple",
-          stages = "fade",
-        })
-    else
-      print("No teaser could be used for APeoplesCalendar.")
-    end
-end
-
 local function table_to_rst(events)
-  result = "================================\nA People's Calendar [" .. os.date("%Y-%m-%d") .. "] \n================================\n "
+  local result = "================================\nA People's Calendar [" .. os.date("%Y-%m-%d") .. "] \n================================\n "
   result = result .. "\n  You can follow the A People's Calendar on https://www.apeoplescalendar.org/ \n  To help add an event to the aPC database or to submit a correction, go here:\n  https://docs.google.com/forms/d/e/1FAIpQLScWvVl15jwOMNyltSzl3elc_mEQzRqamlkKy0HpEvX3fYt_sA/viewform\n "
-  for k,v in ipairs(events) do
+  for _,v in ipairs(events) do
       result = result .. "\n \n" .. v["title"] .. "\n"
-      for i=1,string.len(v["title"]) do 
+      for _=1,string.len(v["title"]) do
         result = result .. "-"
       end
       result = result .. "\n \n"
       result = result .. v["description"] .. "\n \n"
       result = result .. "**Tags:**"
-      for i, tag in ipairs(v["tags"]) do
+      for _, tag in ipairs(v["tags"]) do
           result = result .. " ``" .. tag .. "``,"
       end
       result = result .. "\n \n**Links:**\n"
-      for i, link in ipairs(v["links"]) do
+      for _, link in ipairs(v["links"]) do
           result = result .. "\n  * " .. link
       end
     end
     return result
 end
 
-function M.today()
+function M.today_teaser()
+    local events = {}
+    if test_service_connection() then
+        events = vim.json.decode(fetch_apc_events())
+    end
+    local event = events[ math.random( #events ) ]
+    local teaser = "No server connection..."
+    if event ~= nil then
+        teaser = event["otd"]
+    end
+    teaser = format_up_to_x(teaser, 50)
+    require("notify").notify(teaser .. "\n \nSee all events of today with :APeoplesCalendar", "error", {
+      title = "APeoplesCalendar",
+      timeout = 8000,
+      render = "simple",
+      stages = "fade",
+    })
+end
 
-    local content = table_to_rst(vim.json.decode(fetch_apc_events()))
+function M.today()
+    local content = "No server connection...\nPress `q` to close."
+    if test_service_connection() then
+        local fetched = fetch_apc_events()
+        if fetched then
+            content = table_to_rst(vim.json.decode(fetched))
+        else
+            content = "No events found for today..."
+        end
+    end
     local lines = {}
     for s in content:gmatch("[^\r\n]+") do
       table.insert(lines, s)
@@ -122,7 +161,6 @@ function M.today()
     vim.api.nvim_win_set_cursor(win, {1, 0})
     vim.api.nvim_win_set_option(win, "winhighlight", "Normal:NormalFloat,Border:NormalFloat")
     vim.api.nvim_win_set_option(win, 'wrap', true)
-
 end
 
 return M
